a899aa665b22e2f9d3f0466aab2a7e4e8fe7d1b5
Joseph Tseng <joseph@globalmems.com>
2019-06-11 15:36:46 +0800

    GMP102T driver code and P-/T-forced mode example code
    * GMP102T has different GMP102_CALIB_SCALE_FACTOR definition
    * GMP102T both T&P are measured in raw data mode
    * Compensation is done only for P
    * Example code demo usage of P- and T-forced mode

--- gmp102.h
+++ gmp102.h
@@ -30,17 +30,17 @@
  **************************************************************************/
 
 /*! @file gmp102.h
- *  @brief  GMP102 Sensor Driver Header File 
+ *  @brief  GMP102 Sensor Driver Header File
  *  @author Joseph FC Tseng
  */
- 
+
 #ifndef __GMP102_H__
 #define __GMP102_H__
 
 #include "bus_support.h"
 
 #define GMP102_7BIT_I2C_ADDR		0x6C
-#define GMP102_TEMPERATURE_SENSITIVITY 256  //1 Celsius = 256 code
+#define GMP102_TEMPERATURE_SENSITIVITY  256
 #define GMP102_T_CODE_TO_CELSIUS(tCode) (((float)(tCode)) / GMP102_TEMPERATURE_SENSITIVITY)
 
 //Registers Address
@@ -61,9 +61,14 @@
 #define GMP102_CALIBRATION_REGISTER_COUNT 18
 //Total calibration parameter count: total 9
 #define GMP102_CALIBRATION_PARAMETER_COUNT (GMP102_CALIBRATION_REGISTER_COUNT/2)
-//Soft reset 
+//Soft reset
 #define GMP102_SW_RST_SET_VALUE		0x24
-
+//T-Forced mode
+#define GMP102_T_FORCED_MODE_SET_VALUE	0x08
+//P-Forced mode
+#define GMP102_P_FORCED_MODE_SET_VALUE	0x09
+//Continuous mode
+#define GMP102_CONT_MODE_SET_VALUE	0x0B
 
 /* PID */
 #define GMP102_PID__REG GMP102_REG_PID
@@ -75,6 +80,18 @@
 #define GMP102_DRDY__REG	GMP102_REG_STATUS
 #define GMP102_DRDY__MSK	0x01
 #define GMP102_DRDY__POS	0
+/* Measure CTRL bits */
+#define GMP102_MEAS_CTRL__REG	GMP102_REG_CMD
+#define GMP102_MEAS_CTRL__MSK	0x0F
+#define GMP102_MEAS_CTRL__POS	0
+/* Standby time bits */
+#define GMP102_STANDBY_TIME__REG	GMP102_REG_CMD
+#define GMP102_STANDBY_TIME__MSK	0xF0
+#define GMP102_STANDBY_TIME__POS	4
+/* Raw Data bit */
+#define GMP102_RAW_DATA__REG    GMP102_REG_CONFIG1
+#define GMP102_RAW_DATA__MSK    0x02
+#define GMP102_RAW_DATA__POS    1
 /* P OSR bits */
 #define GMP102_P_OSR__REG       GMP102_REG_CONFIG2
 #define GMP102_P_OSR__MSK       0x07
@@ -98,7 +115,7 @@ typedef enum {
   GMP102_P_OSR_4096 = 0x02,
   GMP102_P_OSR_8192 = 0x03,
   GMP102_P_OSR_16384 = 0x06,
-  GMP102_P_OSR_32768 = 0x07,	
+  GMP102_P_OSR_32768 = 0x07,
 } GMP102_P_OSR_Type;
 
 typedef enum {
@@ -109,8 +126,27 @@ typedef enum {
   GMP102_T_OSR_4096 = 0x02,
   GMP102_T_OSR_8192 = 0x03,
   GMP102_T_OSR_16384 = 0x06,
-  GMP102_T_OSR_32768 = 0x07,	
+  GMP102_T_OSR_32768 = 0x07,
 } GMP102_T_OSR_Type;
+
+typedef enum {
+  GMP102_STANDBY_TIME_0ms = 0,
+  GMP102_STANDBY_TIME_63ms = 1,
+  GMP102_STANDBY_TIME_125ms = 2,
+  GMP102_STANDBY_TIME_188ms = 3,
+  GMP102_STANDBY_TIME_250ms = 4,
+  GMP102_STANDBY_TIME_313ms = 5,
+  GMP102_STANDBY_TIME_375ms = 6,
+  GMP102_STANDBY_TIME_438ms = 7,
+  GMP102_STANDBY_TIME_500ms = 8,
+  GMP102_STANDBY_TIME_563ms = 9,
+  GMP102_STANDBY_TIME_625ms = 10,
+  GMP102_STANDBY_TIME_688ms = 11,
+  GMP102_STANDBY_TIME_750ms = 12,
+  GMP102_STANDBY_TIME_813ms = 13,
+  GMP102_STANDBY_TIME_875ms = 14,
+  GMP102_STANDBY_TIME_938ms = 15,
+} GMP102_STANDBY_TIME_Type;
 
 /*!
  * @brief Read multiple data from the starting regsiter address
@@ -118,7 +154,7 @@ typedef enum {
  * @param u8Addr Starting register address
  * @param pu8Data The data array of values read
  * @param u8Len Number of bytes to read
- * 
+ *
  * @return Result from the burst read function
  * @retval >= 0 Success, number of bytes read
  * @retval -127 Error null bus
@@ -133,7 +169,7 @@ s8 gmp102_burst_read(u8 u8Addr, u8* pu8Data, u8 u8Len);
  * @param u8Addr Starting register address
  * @param pu8Data The data array of values to write
  * @param u8Len Number of bytes to write
- * 
+ *
  * @return Result from the burst write function
  * @retval >= 0 Success, number of bytes write
  * @retval -127 Error null bus
@@ -147,7 +183,7 @@ s8 gmp102_burst_write(u8 u8Addr, u8* pu8Data, u8 u8Len);
  * @brief gmp102 initialize communication bus
  *
  * @param pbus Pointer to the I2C/SPI read/write bus support struct
- * 
+ *
  * @return Result from bus communication function
  * @retval 0 Success
  * @retval -1 Bus communication error
@@ -160,7 +196,7 @@ s8 gmp102_bus_init(bus_support_t* pbus);
  * @brief gmp102 soft reset
  *
  * @param None
- * 
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
@@ -171,11 +207,11 @@ s8 gmp102_soft_reset(void);
 
 /*!
  * @brief Get gmp102 calibration parameters
- *        - Read calibration register AAh~BBh total 18 bytes 
+ *        - Read calibration register AAh~BBh total 18 bytes
  *        - Compose 9 calibration parameters from the 18 bytes
  *
  * @param fCalibParam: the calibration parameter array returned to caller
- * 
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
@@ -184,26 +220,11 @@ s8 gmp102_soft_reset(void);
 s8 gmp102_get_calibration_param(float* fCalibParam);
 
 /*!
- * @brief Get gmp102 calibration parameters for fixed-point compensation
- *        - Read calibration register AAh~BBh total 18 bytes
- *        - Return 9 calibration parameters with fixed-point value and power parts
- *
- * @param s16Value[]: array of the value part of the calibration parameter
- * @param u8Power[]: array of the power part of the calibration parameter
- * 
- * @return Result from bus communication function
- * @retval -1 Bus communication error
- * @retval -127 Error null bus
- *
- */
-s8 gmp102_get_calibration_param_fixed_point(s16 s16Value[], u8 u8Power[]);
-
-/*!
  * @brief gmp102 initialization
  *        Set AAh ~ ADh to 0x00
  *
  * @param None
- * 
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
@@ -213,10 +234,10 @@ s8 gmp102_initialization(void);
 
 
 /*!
- * @brief gmp102 measure temperature
+ * @brief gmp102 T-Forced mode measure temperature
  *
- * @param *ps16T calibrated temperature code returned to caller
- * 
+ * @param *ps16T raw temperature code returned to caller
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
@@ -225,10 +246,10 @@ s8 gmp102_initialization(void);
 s8 gmp102_measure_T(s16* ps16T);
 
 /*!
- * @brief gmp102 measure pressure
+ * @brief gmp102 P-Forced mode measure pressure
  *
  * @param *ps32P raw pressure in code returned to caller
- * 
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
@@ -237,70 +258,74 @@ s8 gmp102_measure_T(s16* ps16T);
 s8 gmp102_measure_P(s32* ps32P);
 
 /*!
- * @brief gmp102 measure pressure and temperature
- *        Read pressure first then commit pressure data conversion for the next call
- *        
- * @param *ps32P raw pressure in code returned to caller
- * @param *ps16T calibrated temperature code returned to caller
- * @param s8WaitPDrdy 1: P wait for DRDY bit set, 0: P no wait
+ * @brief gmp102 pressure compensation
  *
- * 
+ * @param s16T raw temperature in code
+ * @param s32P raw pressure in code
+ * @param fParam[] pressure calibration parameters
+ * @param *pfP_Pa calibrated pressure in Pa returned to caller
+ *
+ * @return None
+ *
+ */
+void gmp102_compensation(s16 s16T, s32 s32P, float fParam[], float* pfP_Pa);
+
+/*!
+ * @brief gmp102 read raw pressure and temperature
+ *        This function just read data registers, thus should
+ *        be called when GMP102 is in the continuous mode that
+ *        data conversion is periodically conducted.
+ *
+ * @param *ps32P raw pressure in code returned to caller
+ * @param *ps16T raw temperature code returned to caller
+ *
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
  *
  */
-s8 gmp102_measure_P_T(s32* ps32P, s16* ps16T, s8 s8PWaitDrdy);
+s8 gmp102_read_P_T(s32* ps32P, s16* ps16T);
 
 /*!
- * @brief gmp102 temperature and pressure compensation
+ * @brief gmp102 set to continuous mode
  *
- * @param s16T calibrated temperature in code
- * @param s32P raw pressure in code
- * @param fParam[] pressure calibration parameters
- * @param *pfT_Celsius calibrated temperature in Celsius returned to caller
- * @param *pfP_Pa calibrated pressure in Pa returned to caller
- * 
- * @return None
+ * @return Result from bus communication function
+ * @retval -1 Bus communication error
+ * @retval -127 Error null bus
  *
  */
-void gmp102_compensation(s16 s16T, s32 s32P, float fParam[], float* pfT_Celsius, float* pfP_Pa);
+s8 gmp102_set_continuous_mode();
 
 /*!
- * @brief gmp102 temperature and pressure compensation, s64 fixed point operation
+ * @brief gmp102 set standby-time for continuous mode
  *
- * @param s16T raw temperature in code
- * @param s32P raw pressure in code
- * @param s16Value[]: array of the value part of the calibration parameter
- * @param u8Power[]: array of the power part of the calibration parameter
- * @param *ps32T_Celsius calibrated temperature in 1/256*Celsius returned to caller
- * @param *ps32P_Pa calibrated pressure in Pa returned to caller
- * 
- * @return None
+ * @param stbyTime standby time to set
+ *
+ * @return Result from bus communication function
+ * @retval -1 Bus communication error
+ * @retval -127 Error null bus
  *
  */
-void gmp102_compensation_fixed_point_s64(s16 s16T, s32 s32P, s16 s16Value[], u8 u8Power[], s32* ps32T_Celsius, s32* ps32P_Pa);
+s8 gmp102_set_standby_time(GMP102_STANDBY_TIME_Type stbyTime);
 
 /*!
- * @brief gmp102 temperature and pressure compensation, s32 fixed point operation
+ * @brief gmp102 set raw/calibrated data
  *
- * @param s16T raw temperature in code
- * @param s32P raw pressure in code
- * @param s16Value[]: array of the value part of the calibration parameter
- * @param u8Power[]: array of the power part of the calibration parameter
- * @param *ps32T_Celsius calibrated temperature in 1/256*Celsius returned to caller
- * @param *ps32P_Pa calibrated pressure in Pa returned to caller
+ * @param isRaw 1 for raw, 0 for calibrated data
  *
- * @return None
+ * @return Result from bus communication function
+ * @retval -1 Bus communication error
+ * @retval -127 Error null bus
  *
  */
-void gmp102_compensation_fixed_point_s32(s16 s16T, s32 s32P, s16 s16Value[], u8 u8Power[], s32* ps32T_Celsius, s32* ps32P_Pa);
+s8 gmp102_set_raw_data(s8 isRaw);
 
 /*!
  * @brief gmp102 set pressure OSR
  *
  * @param osrP OSR to set
- * 
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
@@ -312,7 +337,7 @@ s8 gmp102_set_P_OSR(GMP102_P_OSR_Type osrP);
  * @brief gmp102 set temperature OSR
  *
  * @param osrT OSR to set
- * 
+ *
  * @return Result from bus communication function
  * @retval -1 Bus communication error
  * @retval -127 Error null bus
--- sensor_driver_test.c
+++ sensor_driver_test.c
@@ -26,12 +26,12 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  **************************************************************************/
- 
- /*! @file sensor_driver_test.c
- *  @brief  GMP102 Sensor Driver Test Main Program 
+
+/*! @file sensor_driver_test.c
+ *  @brief  GMP102 Sensor Driver Test Main Program
  *  @author Joseph FC Tseng
  */
- 
+
 #include <stdio.h>
 #include "gmp102.h"
 #include "pSensor_util.h"
@@ -45,64 +45,66 @@
 int main(void)
 {
 
-	s8 s8Res; 
-	bus_support_t gmp102_bus;
-	float fCalibParam[GMP102_CALIBRATION_PARAMETER_COUNT], fT_Celsius, fP_Pa, fAlt_m;
-	s16 s16Value[GMP102_CALIBRATION_PARAMETER_COUNT];
-	u8 u8Power[GMP102_CALIBRATION_PARAMETER_COUNT];
-	s16 s16T;
-	s32 s32P, s32P64_Pa, s32P32_Pa, s32T_Celsius;
+  s8 s8Res;
+  bus_support_t gmp102_bus;
+  float fCalibParam[GMP102_CALIBRATION_PARAMETER_COUNT], fP_Pa, fAlt_m;
+  s16 s16Value[GMP102_CALIBRATION_PARAMETER_COUNT];
+  u8 u8Power[GMP102_CALIBRATION_PARAMETER_COUNT];
+  s16 s16T;
+  s32 s32P;
 
-	/* Add your HW initialization code here
-	...
-	...
-	...
-	...
-	*/
-	
-	/* GMP102 I2C bus setup */
-	bus_init_I2C(&gmp102_bus, GMP102_7BIT_I2C_ADDR); //Initialize I2C bus
-	gmp102_bus_init(&gmp102_bus); //Initailze GMP102 bus to I2C
+  /* Add your HW initialization code here
+     ...
+     ...
+     ...
+     ...
+  */
 
-	/* GMP102 soft reset */
-	s8Res = gmp102_soft_reset();
-	
-	/* Wait 100ms for reset complete */
-	DELAY_MS(100);
-	
-	/* GMP102 get the pressure calibration parameters */
-	s8Res = gmp102_get_calibration_param(fCalibParam);
-	s8Res = gmp102_get_calibration_param_fixed_point(s16Value, u8Power);
-	
-	/* GMP102 initialization setup */
-	s8Res = gmp102_initialization();
+  /* GMP102 I2C bus setup */
+  bus_init_I2C(&gmp102_bus, GMP102_7BIT_I2C_ADDR); //Initialize I2C bus
+  gmp102_bus_init(&gmp102_bus); //Initailze GMP102 bus to I2C
 
-	/* set sea level reference pressure */
-	//If not set, use default 101325 Pa for pressure altitude calculation
-	set_sea_level_pressure_base(102100.f);
-	
+  /* GMP102 soft reset */
+  s8Res = gmp102_soft_reset();
+
+  /* Wait 100ms for reset complete */
+  DELAY_MS(100);
+
+  /* GMP102 get the pressure calibration parameters */
+  s8Res = gmp102_get_calibration_param(fCalibParam);
+
+  /* GMP102 initialization setup */
+  s8Res = gmp102_initialization();
+
+  /* GMP102 set P OSR to 1024 , choose other OSR to meet acutal requirement*/
+  s8Res = gmp102_set_P_OSR(GMP102_P_OSR_1024);
+
+  /* Set to raw data output */
+  s8Res = gmp102_set_raw_data(1);
+
+  /* set sea level reference pressure */
+  //If not set, use default 101325 Pa for pressure altitude calculation
+  set_sea_level_pressure_base(102100.f);
+
   for(;;)
-  {
-		/* Measure P */
-		s8Res = gmp102_measure_P(&s32P);
-		printf("P(code)=%d\r", s32P);
-		
-		/* Mesaure T */
-		s8Res = gmp102_measure_T(&s16T);
-		printf("T(code)=%d\r", s16T);
-		
-		/* Compensation, choose one of the three below in actual implementation*/
-		gmp102_compensation(s16T, s32P, fCalibParam, &fT_Celsius, &fP_Pa);
-		gmp102_compensation_fixed_point_s64(s16T, s32P, s16Value, u8Power, &s32T_Celsius, &s32P64_Pa);
-		gmp102_compensation_fixed_point_s32(s16T, s32P, s16Value, u8Power, &s32T_Celsius, &s32P32_Pa);
-		printf("P(Pa)=%f, %d, %d\r", fP_Pa, s32P64_Pa, s32P32_Pa);
-		printf("T(C)=%f, %f\r", fT_Celsius, s32T_Celsius/256.0);
+    {
+      /* Measure P by P-forced mode*/
+      s8Res = gmp102_measure_P(&s32P);
+      printf("P(code)=%d\r", s32P);
 
-		/* Pressure Altitude */
-		fAlt_m = pressure2Alt(fP_Pa);
-		printf("Alt(m)=%f\r", fAlt_m);
+      /* Mesaure T by T-forced mode*/
+      s8Res = gmp102_measure_T(&s16T);
+      printf("T(code)=%d\r", s16T);
 
-		/* Delay 1 sec */
-		DELAY_MS(1000);
-  }
+      /* P Compensation */
+      gmp102_compensation(s16T, s32P, fCalibParam, &fP_Pa);
+      printf("P(Pa)=%f\r", fP_Pa);
+
+      /* Pressure Altitude */
+      fAlt_m = pressure2Alt(fP_Pa);
+      printf("Alt(m)=%f\r", fAlt_m);
+
+      /* Delay 1 sec */
+      DELAY_MS(1000);
+    }
 }
